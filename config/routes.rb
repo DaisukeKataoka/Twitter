661e2993c97a3ef149885609d4e81793f5b00fac
Daisuke Kataoka <kataoka@dl.soc.i.kyoto-u.ac.jp>
2017-07-22 21:20:27 +0900

    Config Deleted

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :queries
-	devise_for :users
-	root 'pages#index'
-
-	get 'pages/show'
-	get 'tweets/search'
-	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
